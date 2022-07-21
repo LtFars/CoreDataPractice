8d0f5c86c1d76c1d12d91f7a4d5a804044870f7d
Denis Snezhko <mikle4421@mail.ru>
2022-07-21 19:20:51 +0300

    setting-project
    Setup base configuration of my project.

---
+++ CoreDataPractice/Sources/App/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  CoreDataPractice
+//
+//  Created by Denis Snezhko on 21.07.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
